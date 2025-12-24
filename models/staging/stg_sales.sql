e1efdced63648d3791083596252e1e17d4232083
Gregory <85247790+Gregory-Bagramyan@users.noreply.github.com>
2025-12-24 12:56:51 +0100

    added stg file for raw sales casted

---
+++ models/staging/stg_sales.sql
@@ -0,0 +1,22 @@
+WITH raw_sales as (
+    SELECT * FROM ancient-folio-481614-p3.test_raw_data.raw_sales_data
+)
+
+SELECT
+    CAST(`Order ID` as string) as order_id,
+    PARSE_DATE('%d/%m/%Y', `Order Date`) as order_date,
+    PARSE_DATE('%d/%m/%Y', `Ship Date`)as ship_date,
+    CAST(`Customer ID` as string) as customer_id,
+    CAST(`Customer Name` as string) as customer_name,
+    CAST(`Segment` as string) as customer_segment,
+    CAST(`Country` as string) as customer_country,
+    CAST(`City` as string) as customer_city,
+    CAST(`State` as string) as customer_state,
+    CAST(`Postal Code` as string) as customer_postal_code,
+    CAST(`Region` as string) as customer_region,
+    CAST(`Product ID` as string) as product_id,
+    CAST(`Category` as string) as product_category,
+    CAST(`Sub-Category` as string)as product_sub_category,
+    CAST(`Product Name` as string) as product_name,
+    CAST(`Sales` as float) as sales
+FROM raw_sales
